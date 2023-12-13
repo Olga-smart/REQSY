5b492d8772c2980eabf9b110d093633a92739662
Olga_Tarasova <olyaaasyaaa@gmail.com>
2023-12-13 22:02:55 +0700

    Make Company creation page

--- main.js
+++ main.js
@@ -232,7 +232,7 @@ class Modal {
   }
 
   _handleClick(event) {
-    const target = event.target;
+    const { target } = event;
 
     if (target.closest('.js-modal__close-button') || !target.closest('.js-modal__window')) {
       this.close();
@@ -251,3 +251,200 @@ if (modals) {
   });
 }
 /* End Modal */
+
+/* Select */
+class Select {
+  constructor(component) {
+    this._initFields(component);
+    this._handleOutsideClick = this._handleOutsideClick.bind(this);
+    this._attachEventHandlers();
+  }
+
+  toggle() {
+    this._component.classList.toggle('select_opened');
+
+    if (this._component.classList.contains('select_opened')) {
+      this._input.placeholder = 'Выберите форму организации';
+      window.addEventListener('click', this._handleOutsideClick);
+    }
+
+    if (!this._component.classList.contains('select_opened')) {
+      this._input.placeholder = 'Не выбрано';
+      window.removeEventListener('click', this._handleOutsideClick);
+    }
+  } 
+
+  _initFields(component) {
+    this._component = component;
+    this._input = component.querySelector('.js-select__input');
+    this._items = component.querySelectorAll('.js-select__item');
+  }
+
+  _handleInputClick() {
+    this.toggle();
+  }
+
+  _handleItemClick(item) {
+    this._items.forEach((item) => {
+      item.classList.remove('select__item_selected');
+    });
+
+    item.classList.add('select__item_selected');
+    this._input.value = item.textContent;
+    this._input.dispatchEvent(new Event('change'));
+    this.toggle();
+  }
+
+  _handleOutsideClick(event) {
+    const { target } = event;
+    const clickOnSelect = this._component.contains(target);
+
+    if (!clickOnSelect) {
+      this.toggle();
+    }
+  }
+
+  _attachEventHandlers() {
+    this._input.addEventListener('click', this._handleInputClick.bind(this));
+
+    this._items.forEach((item) => {
+      item.addEventListener('click', this._handleItemClick.bind(this, item));
+    })
+  }
+}
+
+const selects = document.querySelectorAll('.js-select');
+if (selects) {
+  selects.forEach((select) => {
+    new Select(select);
+  });
+}
+/* End Select */
+
+/* Company Creation Form */
+class CompanyCreationForm {
+  constructor(component) {
+    this._initFields(component);
+    this._attachEventHandlers();
+  }
+
+  _initFields(component) {
+    this._component = component;
+    this._orgForm = component.querySelector('.js-company-creation-form__org-form');
+    this._name = component.querySelector('.js-company-creation-form__name');
+    this._taxNumber = component.querySelector('.js-company-creation-form__tax-number');
+    this._address =component.querySelector('.js-company-creation-form__address');
+    this._agreement = component.querySelector('.js-company-creation-form__agreement');
+  }
+
+  _handleSubmit(event) {
+    const formIsValid = this._validate();
+
+    if (!formIsValid) {
+      event.preventDefault();
+    }
+  }
+
+  _handleOrgFormChange() {
+    if (this._orgForm.value == 'Физическое лицо') {
+      this._taxNumber.closest('.js-form-control').style.display = 'none';
+      this._address.closest('.js-form-control').style.display = 'none';
+    } else {
+      this._taxNumber.closest('.js-form-control').style.display = 'block';
+      this._address.closest('.js-form-control').style.display = 'block';
+    }
+  }
+
+  _validate() {
+    let formIsValid = true;
+
+    const orgFormValue = this._orgForm.value.trim();
+    if (orgFormValue === '') {
+      formIsValid = false;
+      this._setErrorFor(this._orgForm, 'Выберите форму организации');
+    } else {
+      this._removeErrorFor(this._orgForm);
+    }
+
+    const nameValue = this._name.value.trim();
+    if (nameValue === '') {
+      formIsValid = false;
+      this._setErrorFor(this._name, 'Поле обязательно для заполнения');
+    } else if (nameValue.length < 2) {
+      formIsValid = false;
+      this._setErrorFor(this._name, 'Слишком мало символов');
+    }  else {
+      this._removeErrorFor(this._name);
+    }
+
+    if (orgFormValue === 'Юридическое лицо') {
+      const taxNumberValue = this._taxNumber.value.trim();
+      if (taxNumberValue === '') {
+        formIsValid = false;
+        this._setErrorFor(this._taxNumber, 'Поле обязательно для заполнения');
+      } else if (!this._isTaxNumber(taxNumberValue)) {
+        formIsValid = false;
+        this._setErrorFor(this._taxNumber, 'Введите правильный ИНН');
+      } else {
+        this._removeErrorFor(this._taxNumber);
+      }
+
+      const addressValue = this._address.value.trim();
+      if (addressValue === '') {
+        formIsValid = false;
+        this._setErrorFor(this._address, 'Поле обязательно для заполнения');
+      } else {
+        this._removeErrorFor(this._address);
+      }
+    }  
+
+    const agreementIsAccepted = this._agreement.checked;
+    if (!agreementIsAccepted) {
+      formIsValid = false;
+      this._setErrorFor(this._agreement, 'Нужно принять соглашение');
+    } else {
+      this._removeErrorFor(this._agreement);
+    }
+
+    return formIsValid;
+  }
+
+  _isTaxNumber(value) {
+    return /^[0-9]{12}$/.test(value);
+  }
+
+  _setErrorFor(input, message) {
+    const formControl = input.closest('.js-form-control');
+
+    const previousErrorMessage = formControl.querySelector('.form-control__error-message');
+    if (previousErrorMessage) {
+      previousErrorMessage.remove();
+    }
+    const errorMessage = document.createElement('div');
+    errorMessage.className = 'form-control__error-message';
+    errorMessage.textContent = message;
+    
+    formControl.append(errorMessage);
+    formControl.classList.add('form-control_error');
+  }
+
+  _removeErrorFor(input) {
+    const formControl = input.closest('.js-form-control');
+    const errorMessage = formControl.querySelector('.form-control__error-message');
+    if (errorMessage) {
+      errorMessage.remove();
+      formControl.classList.remove('form-control_error');
+    }
+  }
+
+  _attachEventHandlers() {
+    this._component.addEventListener('submit', this._handleSubmit.bind(this));
+    this._orgForm.addEventListener('change', this._handleOrgFormChange.bind(this));
+  }
+}
+
+const companyCreationForm = document.querySelector('.js-company-creation-form');
+if (companyCreationForm) {
+  new CompanyCreationForm(companyCreationForm);
+}
+/* End Company Creation Form */
