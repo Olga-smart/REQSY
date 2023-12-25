af794ea6800b48312bf809988a0099c3ffff6c1b
Olga_Tarasova <olyaaasyaaa@gmail.com>
2023-12-25 10:31:31 +0700

    Make Notifications page

--- main.js
+++ main.js
@@ -1055,3 +1055,52 @@ if (requestPagePhotoGroup) {
   });
 }
 // #endregion Request Page Photo Viewer
+
+// #region Date Picker
+class DatePicker {
+  constructor(component) {
+    this._initFields(component);
+  }
+  
+  _initFields(component) {
+    this._component = component;
+    this._input = component.querySelector('.js-date-picker__input');
+    this._datePicker = this._initDatePicker();
+  }
+
+  _initDatePicker() {
+    const cancelButton = {
+      content: 'Отмена',
+      className: 'button date-picker__cancel-button',
+      onClick: (datePicker) => {
+        datePicker.clear();
+      },
+    }
+
+    const choiceButton = {
+      content: 'Выбрать',
+      className: 'button',
+      onClick: (datePicker) => {
+        datePicker.hide();
+      },
+    }
+
+    return new AirDatepicker(this._input, {
+      navTitles: {
+        days: 'MMMM yyyy',
+      },
+      buttons: [cancelButton, choiceButton],
+      prevHtml: '<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 9.25L1 5L5.5 0.75" stroke="#131A29" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+      nextHtml: '<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 9.25L5.5 5L1 0.75" stroke="#131A29" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+      maxDate: new Date(),
+    });
+  }
+}
+
+const datePickers = document.querySelectorAll('.js-date-picker');
+if (datePickers) {
+  datePickers.forEach((picker) => {
+    new DatePicker(picker);
+  });
+}
+// #endregion Date Picker
